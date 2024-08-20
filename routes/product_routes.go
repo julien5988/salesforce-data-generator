611fb5463011fba33952e42e4338d35dfb7383b3
Julien <julien.thoumy@gmail.com>
2024-08-20 22:31:23 +0200

    Add product routes

---
+++ routes/product_routes.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"net/http"
+	"salesforce-data-generator/generator"
+	"salesforce-data-generator/utils"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// handleGenerate handles form submission and generates the CSV file
+func handleGenerateProduct(c *gin.Context) {
+	countStr := c.PostForm("count")
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid count parameter"})
+		return
+	}
+
+	// Generate fake accounts
+	accounts, err := generator.GenerateProducts(count)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating accounts"})
+		return
+	}
+
+	// Define the path for the CSV file
+	filePath := "./static/lead.csv"
+	err = utils.WriteCSV(filePath, accounts)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error writing CSV file"})
+		return
+	}
+
+	// Serve the CSV file as a downloadable attachment
+	c.Header("Content-Disposition", "attachment; filename=accounts.csv")
+	c.Header("Content-Type", "text/csv")
+	c.File(filePath)
+}
